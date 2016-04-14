646bebb22b05e9624bc6696048d97029762ffb72
panos <panospet@gmail.com>
2016-04-14 21:33:51 +0300

    Managed user input urls

--- download_url_images.py
+++ download_url_images.py
@@ -1,13 +1,34 @@
+import os
 import re
 import urllib2
 import urllib
 
-url = 'http://' + raw_input("Please enter your url: ")
+
+def ask_user_for_url():
+    input_url = raw_input("Please enter your url: ")
+    if not input_url.startswith('http'):
+        full_url = 'http://' + input_url
+    else:
+        full_url = input_url
+    return full_url, input_url
+
+
+def is_url_up(full_url):
+    if urllib.urlopen(full_url).getcode() == 200:
+        return True
+    else:
+        return False
+
+
+url, input_url = ask_user_for_url()
+while not is_url_up(url):
+    print "The url you gave is DOWN!!"
+    url, input_url = ask_user_for_url()
 
 page_source = urllib2.urlopen(url)
 page_source = page_source.read()
 
-extensions_wanted = ['.png', '.jpg', '.jpeg', '.bmp']
+extensions_wanted = ['.png', '.jpg', '.jpeg', '.bmp', '.gif']
 
 matches = re.findall(r'\ssrc="([^"]+)"', page_source)
 images = []
@@ -19,7 +40,8 @@ for i in matches:
         else:
             images.append(url + i)
 
-urllib.urlretrieve(images[0], images[0].split('/')[-1])
+if not os.path.exists(input_url):
+    os.makedirs(input_url)
 
 for i in images:
-    urllib.urlretrieve(i, i.split('/')[-1])
+    urllib.urlretrieve(i, input_url + '/' + i.split('/')[-1])
